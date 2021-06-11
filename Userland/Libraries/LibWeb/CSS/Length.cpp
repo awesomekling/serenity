d710f0f9525066e35e8ecd34b50dc2fc861c407d
Tobias Christiansen <tobi@tobyase.de>
2021-06-12 00:50:16 +0200

    LibWeb: Add calc() resolution to CSS::Length

--- Userland/Libraries/LibWeb/CSS/Length.cpp
+++ Userland/Libraries/LibWeb/CSS/Length.cpp
@@ -1,9 +1,13 @@
 /*
  * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Tobias Christiansen <tobi@tobyase.de>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/NonnullOwnPtr.h>
+#include <AK/NonnullOwnPtrVector.h>
+#include <AK/Variant.h>
 #include <LibWeb/CSS/Length.h>
 #include <LibWeb/DOM/Document.h>
 #include <LibWeb/HTML/HTMLHtmlElement.h>
@@ -38,6 +42,86 @@ float Length::relative_length_to_px(const Layout::Node& layout_node) const
         VERIFY_NOT_REACHED();
     }
 }
+
+float Length::resolve_calculated_value(const Layout::Node& layout_node, float reference_for_percent) const
+{
+    // FIXME: Make this more error-friendly.
+    auto resolve_calc_value = [&layout_node, &reference_for_percent](CalculatedStyleValue::CalcValue const& calc_value) -> float {
+        return calc_value.visit(
+            [](float value) { return value; },
+            [&](Length length) {
+                return length.resolved_or_zero(layout_node, reference_for_percent).to_px(layout_node);
+            },
+            [](NonnullOwnPtr<CalculatedStyleValue::CalcSum>&) {
+                TODO();
+                return 0.0f;
+            },
+            [](auto&) {
+                VERIFY_NOT_REACHED();
+                // The compiler will whine when this isn't here although it's unreachable
+                return 0.0f;
+            });
+    };
+
+    auto resolve_calc_number_value = [](CalculatedStyleValue::CalcNumberValue const& number_value) {
+        return number_value.visit(
+            [](float number) { return number; },
+            [](NonnullOwnPtr<CalculatedStyleValue::CalcNumberSum>&) {
+                // FIXME: Allow for nested calc()
+                TODO();
+                return 0.0f;
+            });
+    };
+
+    auto resolve_calc_product = [&resolve_calc_value, &resolve_calc_number_value](NonnullOwnPtr<CalculatedStyleValue::CalcProduct> const& calc_product) {
+        auto value = resolve_calc_value(calc_product->first_calc_value);
+
+        for (auto& additional_value : calc_product->zero_or_more_additional_calc_values) {
+            additional_value.value.visit(
+                [&additional_value, &resolve_calc_value, &value](CalculatedStyleValue::CalcValue const& calc_value) {
+                    if (additional_value.op != CalculatedStyleValue::CalcProductPartWithOperator::Multiply)
+                        VERIFY_NOT_REACHED();
+                    auto resolved_value = resolve_calc_value(calc_value);
+                    value *= resolved_value;
+                },
+                [&additional_value, &value](CalculatedStyleValue::CalcNumberValue const& calc_number_value) {
+                    if (additional_value.op != CalculatedStyleValue::CalcProductPartWithOperator::Divide)
+                        VERIFY_NOT_REACHED();
+                    calc_number_value.visit(
+                        [&value](float number) {
+                            value /= number;
+                        },
+                        [](NonnullOwnPtr<CalculatedStyleValue::CalcNumberSum>&) {
+                            TODO();
+                        });
+                });
+        }
+
+        return value;
+    };
+
+    auto resolve_calc_sum = [&resolve_calc_product](NonnullOwnPtr<CalculatedStyleValue::CalcSum> const& calc_sum) {
+        auto value = resolve_calc_product(calc_sum->first_calc_product);
+
+        for (auto& additional_product : calc_sum->zero_or_more_additional_calc_products) {
+            auto additional_value = resolve_calc_product(additional_product.calc_product);
+            if (additional_product.op == CalculatedStyleValue::CalcSumPartWithOperator::Operation::Add)
+                value += additional_value;
+            else
+                value -= additional_value;
+        }
+
+        return value;
+    };
+
+    if (!m_calculated_style)
+        return 0.0f;
+
+    auto& expression = m_calculated_style->expression();
+
+    auto length = resolve_calc_sum(expression);
+    return length;
+};
 
 const char* Length::unit_name() const
 {
