a8abfeeff37fdc6219c68bbd3cbd9e00e5cda23c
frhun <28605587+frhun@users.noreply.github.com>
2021-07-07 17:29:07 +0200

    Adjust to clang format

--- Userland/Libraries/LibGfx/Rect.h
+++ Userland/Libraries/LibGfx/Rect.h
@@ -283,7 +283,7 @@ public:
     [[nodiscard]] Point<T> clamp(Point<T> const& input) const
     {
         return Point<T>(AK::clamp<T>(input.x(), m_location.x(), m_location.x() + m_size.width()),
-                        AK::clamp<T>(input.y(), m_location.y(), m_location.y() + m_size.height()));
+            AK::clamp<T>(input.y(), m_location.y(), m_location.y() + m_size.height()));
     }
 
     template<typename Container>
